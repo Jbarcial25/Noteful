3056d2d458a6ebb9d5c1d6d676253b6b47dc0944
Jasper Barcial <Jbarcial25@gmail.com>
2022-06-08 05:15:43 -0700

    deploying part 2

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // 'mongodb+srv://noteful-app:notefulclone123@noteful-app.fthcj.mongodb.net/test'
-mongoose.connect( 'mongodb+srv://noteful-app:notefulclone123@noteful-app.fthcj.mongodb.net/test',
+mongoose.connect( 'mongodb+srv://Kaayching:Jspr1233@cluster0.w2p5d.mongodb.net/test',
     process.env.MONGO_URI || 
     {
         useNewUrlParser: true,
